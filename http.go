36dd95217fe0ae7f5dcf8cfd6dcaa5a46fa86079
Igor Klipach <klipach@gmail.com>
2024-10-21 20:37:45 +0200

    Fix GameId

--- http.go
+++ http.go
@@ -3,7 +3,7 @@ package matchguru
 type MessageRequest struct {
 	Message string `json:"message"`
 	ChatID  int    `json:"chat_id"`
-	GameId  int    `json:"game_id"`
+	GameID  int    `json:"game_id"`
 }
 
 type MessageResponse struct {
